605c46320aa24c68be93f5c6bf60cd70b7ee04d8
Joseph D. Purcell <josephdpurcell@gmail.com>
2015-05-31 12:21:59 -0500

    Add admin and demo openrc scripts for creds.

---
+++ node0-controller/provisioning/keystone/admin-openrc.sh
@@ -0,0 +1,7 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=pass
+export OS_AUTH_URL=http://node0-controller.joepcloud.local:35357/v3
---
+++ node0-controller/provisioning/keystone/demo-openrc.sh
@@ -0,0 +1,7 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=pass
+export OS_AUTH_URL=http://node0-controller.joepcloud.local:5000/v3

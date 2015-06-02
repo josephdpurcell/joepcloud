cb190a1620bd1a203b9c8c0c95a55c020cc0a437
Joseph D. Purcell <josephdpurcell@gmail.com>
2015-06-01 21:36:02 -0500

    Install network service on node2.

--- node2-compute/provisioning/bootstrap.sh
+++ node2-compute/provisioning/bootstrap.sh
@@ -12,4 +12,18 @@ cp $DIR/nova/nova.conf /etc/nova/nova.conf
 service nova-compute restart
 rm -f /var/lib/nova/nova.sqlite
 
+# Setup Network service.
+# Note: this is also called Neutron.
+# 1. Update sysctl.conf
+cp $DIR/sysctl.conf /etc/sysctl.conf
+sysctl -p
+# 2. Install Neutron.
+DEBIAN_FRONTEND=noninteractive apt-get -y install neutron-plugin-ml2 neutron-plugin-openvswitch-agent
+# 3. Update neutron config.
+cp $DIR/neutron/neutron.conf /etc/neutron/neutron.conf
+cp $DIR/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugins/ml2/ml2_conf.ini
+# 4. Restart services.
+service openvswitch-switch restart
+service nova-compute restart
+
 touch /tmp/provisioned
